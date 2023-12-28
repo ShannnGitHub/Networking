b1f39af18f18265b7e4bc4c4a84fef5d16b1a671
ShannnGitHub <143635518+ShannnGitHub@users.noreply.github.com>
2023-12-29 03:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'txt', 'mp4'}  # Add or modify allowed file extensions
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -8,19 +14,26 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Check if the post request has the file part
-    if 'image' not in request.files or 'text' not in request.files or 'video' not in request.files:
+    # Check if at least one file is present in the request
+    if 'image' not in request.files and 'text' not in request.files and 'video' not in request.files:
         return 'No file part'
 
-    image = request.files['image']
-    text = request.files['text']
-    video = request.files['video']
+    # Check each file individually and handle it if present
+    for file_type in ['image', 'text', 'video']:
+        if file_type in request.files:
+            file = request.files[file_type]
 
-    # You can handle each file as needed (save to disk, process, etc.)
-    # For simplicity, we'll just print the file names here
-    print("Image File Name:", image.filename)
-    print("Text File Name:", text.filename)
-    print("Video File Name:", video.filename)
+            # Validate file type
+            if not allowed_file(file.filename):
+                return f'Invalid {file_type} file type'
+
+            # Validate file size (adjust the limit as needed)
+            if len(file.read()) > 5 * 1024 * 1024:  # 5 MB limit
+                return f'{file_type} file size exceeds the limit'
+
+            # Save the file to a secure location (modify the path as needed)
+            file.save(os.path.join('/path/to/uploaded/files/', file.filename))
+            print(f'{file_type.capitalize()} File Name:', file.filename)
 
     return 'Files uploaded successfully!'
 
